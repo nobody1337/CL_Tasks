1fa8f0d1faac7dc229d7055579fa94a85ebbf32b
nobody1337 <98647065+nobody1337@users.noreply.github.com>
2022-04-19 16:12:43 +0200

    Erste Aufgabe erledigt

---
+++ src/main/java/CodewarsTasks/Banjo/Banjo.java
@@ -0,0 +1,21 @@
+package CodewarsTasks.Banjo;
+/*Codewars Aufgabe: Are You Playing Banjo?
+* Create a function which answers the question "Are you playing banjo?".
+* If your name starts with the letter "R" or lower case "r", you are playing banjo!
+* The function takes a name as its only argument, and returns one of the following strings:
+* name + "plays banjo"
+* name + "does not play banjo"
+*/
+
+
+public class Banjo {
+    public static String areYouPlayingBanjo(String name){
+
+        if (name.toUpperCase().startsWith("R")){
+            return name + " plays banjo";
+        }
+        else{
+            return name + " does not play banjo";
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import CodewarsTasks.Banjo.Banjo;
+
 public class Main {
+
     public static void main(String[] args) {
+        System.out.println(Banjo.areYouPlayingBanjo("Tom"));
+        System.out.println(Banjo.areYouPlayingBanjo("Rita"));
+        System.out.println(Banjo.areYouPlayingBanjo("Axel"));
+        System.out.println(Banjo.areYouPlayingBanjo("Rapunzel"));
+        System.out.println("--------");
+        System.out.println(Banjo.areYouPlayingBanjo("tom"));
+        System.out.println(Banjo.areYouPlayingBanjo("rita"));
+        System.out.println(Banjo.areYouPlayingBanjo("axel"));
+        System.out.println(Banjo.areYouPlayingBanjo("rapunzel"));
 
     }
 }
